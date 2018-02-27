5a75d50356f33995545d3639a02a9d318acebe51
Mark Kovacs <drmarkaron@gmail.com>
2018-02-27 22:00:55 +0100

    feat(Dominoes) make file, look at code

--- src/BlogPost.java
+++ src/BlogPost.java
@@ -1,2 +1,22 @@
 public class BlogPost {
+  private String authorname;
+  private String title;
+  private String text;
+  private String publicationDate;
+
+  public static void main(String[] args) {
+    BlogPost blogPost01 = new BlogPost();
+    blogPost01.authorname = "Jhon Doe";
+    blogPost01.title = "Lorem Ipsum";
+    blogPost01.text ="Lorem ipsum dolor sit amet.";
+    blogPost01.publicationDate="2000.05.04.";
+
+    BlogPost blogPost02 = new BlogPost();
+    blogPost02.authorname="Tim Urban";
+    blogPost02.title="Wait but why";
+    blogPost02.text ="A popular long-form, stick-figure-illustrated blog about almost everything.";
+    blogPost02.publicationDate="2010.10.10.";
+  }
+
+
 }
