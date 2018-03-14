e807ef5df16fea428ac1ff5073629d707f6685bd
Mark Kovacs <drmarkaron@gmail.com>
2018-03-14 10:47:08 +0100

    feat(practice exam): multiplication start

---
+++ src/practicetest/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package practicetest;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Which number would you like to multiply?");
+    String inputNum = scanner.nextLine();
+    
+  }
+}
